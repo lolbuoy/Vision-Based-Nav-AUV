66e4a899e8e6b26e8665a980791e60f5cea17683
tanu-04 <129984837+tanu-04@users.noreply.github.com>
2024-01-14 19:50:13 +0530

    Update inf3.py

--- inf3.py
+++ inf3.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 model = YOLO("/home/tanu/Desktop/me/predicts/all(train71)/weights/best.pt")
 
 try:
-    with open('/home/tanu/Desktop/yolov8/me/new1/log.txt', 'a') as log_file:
+    with open('/home/tanu/Desktop/yolov8/me/new1/log.txt', 'a+') as log_file:
         log_file.write("Starting object detection process...\n")
 
-        results = model.predict(source=4, project='/home/tanu/Desktop/yolov8/me/new1', name='test1', exist_ok=True, save_crop=True,stream=True)
+        results = model.predict(source='/home/tanu/Desktop/yolov8/camera', project='/home/tanu/Desktop/yolov8/me/new', name='test1', exist_ok=True, save_crop=True,stream=True)
 
         for i, result in enumerate(results):
             log_file.write(f"Processing result {i}\n")
@@ -17,9 +17,12 @@ try:
                 coords = [round(x) for x in coords]
                 center_x = (coords[0] + coords[2]) / 2
                 center_y = (coords[1] + coords[3]) / 2
+                x_min, y_min, x_max, y_max = coords
 
                 # Log coordinates to the log file
-                log_file.write(f"Object type: {class_name}, Center_X: {center_x}, Center_Y: {center_y}\n")
+                log_file.write(f"Object type: {class_name}, Center_X: {center_x}, Center_Y: {center_y}, Coordinates: {coords},\n")
+                log_file.write(f"X_min: {x_min}, Y_min: {y_min}, X_max: {x_max}, Y_max: {y_max}\n")
+
 
 except Exception as e:
     print(f"Error occurred: {e}")
